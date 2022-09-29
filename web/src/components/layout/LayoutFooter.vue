001ceed07bb68982baa6b4c9f677f3caa2a7578d
abigmiu <suraelax@gmail.com>
2022-09-29 16:37:22 +0800

    1

--- web/src/components/layout/LayoutFooter.vue
+++ web/src/components/layout/LayoutFooter.vue
@@ -1,9 +1,13 @@
 <template>
     <div class="footer-wrapper">
-
+        <div class="action-item"></div>
+        <div class="action-item"></div>
+        <div class="action-item"></div>
     </div>
 </template>
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { Home as HomeIcon, AddOne, Me as MeIcon } from '@icon-park/vue-next'
+</script>
 <style lang="scss" scoped>
 .footer-wrapper {
     position: fixed;
@@ -12,6 +16,10 @@
     bottom: 20px;
     padding: 10px 20px;
     border-radius: 16px;
-    background-color: black;
+    background-color: #fff;
+    box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
 }
 </style>
