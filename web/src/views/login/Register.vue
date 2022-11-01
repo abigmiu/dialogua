34a8142ce8f881df6770f7cf3b091684b1bb1a43
abigmiu <suraelax@gmail.com>
2022-11-01 11:18:10 +0800

    style: Register.vue

--- web/src/views/login/Register.vue
+++ web/src/views/login/Register.vue
@@ -1,22 +1,10 @@
 <template>
     <div class="h-full flex flex-col justify-center items-center p-5">
         <h1 class="mb-2 text-lg">注册</h1>
-        <van-field
-            placeholder="昵称"
-            v-model.trim="formData.nickname"
-        ></van-field>
+        <van-field placeholder="昵称" v-model.trim="formData.nickname"></van-field>
         <van-field placeholder="邮箱" v-model.trim="formData.email"></van-field>
-        <van-field
-            placeholder="密码"
-            v-model.trim="formData.password"
-        ></van-field>
-        <van-button
-            style="margin-top: 20px"
-            type="primary"
-            block
-            :loading="submitLoading"
-            @click="onSubmit"
-        >
+        <van-field placeholder="密码" v-model.trim="formData.password" type="password"></van-field>
+        <van-button style="margin-top: 20px" type="primary" block :loading="submitLoading" @click="onSubmit">
             注册
         </van-button>
         <van-button style="margin-top: 20px" block @click="onCancel">
@@ -26,18 +14,21 @@
 </template>
 
 <script setup lang="ts">
+import type { IUserInfo } from '@/types/User';
+
 import { reactive, ref } from 'vue';
 import { Toast } from 'vant';
 import { useRouter } from 'vue-router';
-import { http } from '@/utils/http';
+
 import { useUserStore } from '@/store/user';
-import { IUserInfo } from '@/types/User';
+
+import { http } from '@/utils/http';
+
 
 const router = useRouter();
 const userStore = useUserStore();
 
 const submitLoading = ref(false);
-
 const formData = reactive({
     email: '',
     password: '',
@@ -46,28 +37,18 @@ const formData = reactive({
 
 const onSubmit = async () => {
     try {
-        if (
-            !/\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*/.test(formData.email)
-        ) {
-            return Toast({
-                message: '邮箱格式不正确',
-            });
+        if (!/\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*/.test(formData.email)) {
+            return Toast('邮箱格式不正确');
         }
-
         if (formData.password.length < 6) {
-            return Toast({
-                message: '密码不能小于6位',
-            });
+            return Toast('密码不能小于6位');
         }
+        if (!/^[\u4e00-\u9fa50-9A-Za-z0-9]{2,10}$/.test(formData.nickname)) {
+            return Toast('昵称格式为2-10位中英文数字组合');
+        }
+
         submitLoading.value = true;
-
-        if (formData.nickname.length < 2 || formData.nickname.length > 10) {
-            return Toast({
-                message: '昵称2-10个字',
-            });
-        }
-
-        const res: IUserInfo = await http.post('user', formData);
+        const res = await http.post<IUserInfo>('user', formData);
         userStore.setUser(res);
         Toast('注册成功');
         router.replace('/');
@@ -80,5 +61,3 @@ const onCancel = () => {
     router.back();
 };
 </script>
-
-<style scoped></style>
